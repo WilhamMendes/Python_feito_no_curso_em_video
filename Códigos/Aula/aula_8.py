31711d867b533949022be7296e7bb2b16aa64982
Wilham Lamine Queita Mendes <mendeswil710@gmail.com>
2022-06-11 19:09:03 -0300

    Aula_8

---
+++ Códigos/Aula/aula_8.py
@@ -0,0 +1,8 @@
+#import math
+from math import sqrt
+num = int(input("\nDigite um valor: "))
+
+#raiz = math.sqrt(num)
+raiz = sqrt(num)
+
+print("A raiz quadrada do número {} é {:.2f}" .format(num, raiz))
