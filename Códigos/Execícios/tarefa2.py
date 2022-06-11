3b87eed130b481567231dc4ca58529fbf94e6a8c
Wilham Lamine Queita Mendes <mendeswil710@gmail.com>
2022-06-11 14:35:39 -0300

    Creat tarefa 2

---
+++ Códigos/Execícios/tarefa2.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plot
+import numpy as np
+
+pi = np.pi/3
+n = 4000
+tx = 2
+
+t = np.linspace(-2.0, tx, n)
+
+x1 = (20.0 * np.sin(2.0*np.pi) * (1000*t) - pi) * np.exp((-a)*t);
+
