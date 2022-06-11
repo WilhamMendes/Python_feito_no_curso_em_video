add25fa88fc9600b0bd6a61966aefca53c71818e
Wilham Lamine Queita Mendes <mendeswil710@gmail.com>
2022-06-11 20:45:29 -0300

    Aula_8

--- Códigos/Aula/aula_8.py
+++ Códigos/Aula/aula_8.py
@@ -1,3 +1,6 @@
+"""
+# Importar biblioteca
+
 #import math
 from math import sqrt
 num = int(input("\nDigite um valor: "))
@@ -6,3 +9,15 @@ num = int(input("\nDigite um valor: "))
 raiz = sqrt(num)
 
 print("A raiz quadrada do número {} é {:.2f}" .format(num, raiz))
+"""
+
+
+# Randomizar um número inteiro
+import random
+num = random.randint(1, 10)
+print("\nO número randomizado é {}\n\n" .format(num))
+
+
+# Utilizando emoji
+import emoji
+print(emoji.emojize("\n\nOlá mundo :earth_americas: \n\n\n", use_aliases=True))
