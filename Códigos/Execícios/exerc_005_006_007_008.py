ecb9c279d61e28808dbef5e06c7edcad9b7c6ce5
Wilham Lamine Queita Mendes <mendeswil710@gmail.com>
2022-06-11 15:44:06 -0300

    Creat Exercício de 005 a 009

---
+++ Códigos/Execícios/exerc_005_006_007_008.py
@@ -0,0 +1,37 @@
+
+"""
+# Exercício 005
+valor = int(input("\nEntre com um valor: "))
+print(" Você digitou o número: {}\n O Antecessor de {} é: {}\n O sucessor de {} é: {}".format(valor, valor, valor-1, valor, valor+1))
+
+# Exercício 006
+print("\n O dobro de {} é: {}\n O triplo de {} é: {}\n O quadrado de {} é: {}" .format(valor, valor * 2, valor, valor * 3, valor, valor ** 2))
+"""
+
+"""
+# Exercício 007
+nota1 = float(input("\nEntre com a primeira nota: "))
+nota2 = float(input("Entre com a senda nota: "))
+
+media = (nota1 + nota2)/2
+
+print("\nA média deste aluno é: {}\n" .format(media))
+"""
+
+
+"""
+# Exercício 008
+medida = float(input("Informe a medida em metro: "))
+cm = medida * 100
+mm = medida * 1000
+print("{} metros corresponde {} centimetros.\n {} metros corresponde {} milimetros.\n" .format(medida, cm, medida, mm))
+"""
+
+# Exercício 009
+num = int(input("\nDigite um valor inteiro: "))
+print("\nA tabuda de {}" .format(num))
+
+print(" {} x 1 = {}\n {} x 2 = {}\n {} x 3 = {}" .format(num, num * 1, num, num * 2, num, num * 3))
+print(" {} x 4 = {}\n {} x 5 = {}\n {} x 6 = {}" .format(num, num * 4, num, num * 5, num, num * 6))
+print(" {} x 7 = {}\n {} x 8 = {}\n {} x 9 = {}\n {} x 10 = {}" .format(num, num * 7, num, num * 8, num, num * 9, num, num * 10))
+
