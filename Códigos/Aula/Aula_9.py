36097c88a2d2b743a381f92b129ba5ca56929b71
Wilham Lamine Queita Mendes <mendeswil710@gmail.com>
2022-06-16 21:22:11 -0300

    Update Aula_9.py

--- Códigos/Aula/Aula_9.py
+++ Códigos/Aula/Aula_9.py
@@ -21,14 +21,45 @@ print("\n\nSegue a frase modificada:")
 print(frase.upper())
 
 
-# Contar quantas vezes a letra "O" apareceu na frase
+# Contar quantas vezes a letra "O" apareceu na frase  modificada
 print("\n\nA letra 'O' apareceu:")
 print(frase.upper().count('O'))
 
 # Remover os espaços a esquesda e a direita da frase
-fras = '      Curso em vídeo Python    '
+fras = '      Curso em vídeo Python      '
 
 print("\n\nQuantidade de pequenos espaços e letras contidas na fras:")
 print(len(fras))
 
-print("Quantidade")
+print("\n\nQuantidade de pequenos espaços, sendo que os espaços antes e depois da frase foram eleiminados:")
+print(len(fras.strip()))
+
+
+# Replace, isto é, subtituir, porém temporariamente
+print('\n\nReplace, isto é, subtituir, porém temporariamente.\nObs. a diferença')
+print(frase.replace('Python', 'Android'))
+print(frase)
+
+
+# Replace, isto é, subtituir definitivamente
+print('\n\nReplace, isto é, subtituir definitivamente')
+frase = frase.replace('Python', 'Android')
+print(frase)
+print(frase, '\n\n')
+
+
+# Verifica se existe uma palavra numa frase
+print('Android' in frase)
+
+
+# Ache uma palavra numa frase
+print(frase.find('em'))
+print(frase.lower().find('android'))
+
+
+# Separar uma frase
+print('\n\n')
+print(frase.split())
+dividido = frase.split()
+print("\n\nImprime 2ª letra da 3ª palavra")
+print(dividido[2] [1])
