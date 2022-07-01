4d8a0effac1ba67a17e50a55b84d04cccc7b5c5b
Wilham Lamine Queita Mendes <mendeswil710@gmail.com>
2022-07-01 01:00:14 -0300

    Update exerc_022_a_027.py

--- Códigos/Execícios/exerc_022_a_027.py
+++ Códigos/Execícios/exerc_022_a_027.py
@@ -20,6 +20,23 @@ print(num[0], 'Milhar/es')
 """
 
 
+
+"""
 # Exercício 024
 cidade = str(input("Digite o nome de qualquer cidade: "))
-print('Santo' in cidade[4])
+print('Santo' in cidade)
+"""
+
+
+"""
+#Exercício 025
+nome = str(input("Qual é o seu nome? "))
+print("Silva está no nome digitado? R: ", 'Silva' in nome)
+"""
+
+#Exercício 026
+frase = str(input("Digite uma frase: "))
+frase = frase.upper()
+print("A letra A aparece ", frase.count('A'), "vezes na frase.")
+print("A letra A aparece pela 1ª Vez na posição {}" .format(frase.find('A')+1))
+print("A letra A apareceu pela última vez na posição {}" .format(frase.find('A')+1))
