193fd5fe90f1c294ba464706e0971b82121b1e9e
Wilham Lamine Queita Mendes <mendeswil710@gmail.com>
2022-08-09 02:57:15 -0300

    Update exerc_028_a_036.py

--- Códigos/Execícios/exerc_028_a_036.py
+++ Códigos/Execícios/exerc_028_a_036.py
@@ -1,18 +1,27 @@
 
 
+
 """
 # Exercício 028
-from random import Random
-import random
+from random import randint
+from time import sleep
 
-print("\nOlá, vamos fazer um joginho. \nVocê tem que digitar um número inteiro de 0 a 5, para verificar se é o mesmo número que o computador escolheu.\n")
-digite = int(input("Digite um numero inteiro de 0 a 5: "))
-num_escolhido = random.randint(0, 5)
+print("\n")
+print("*" *20)
+print("Olá, vamos fazer um joginho. \nVou pensar em um número entre 0 e 5. Tente advinhar...")
+print("*" *20)
+
+digite = int(input("\nDigite um número inteiro de 0 a 5: "))
+print("Processando...\n")
+
+sleep(2)
+num_escolhido = randint(0, 5)
 
 if digite == num_escolhido:
     print("PARABÉNS, Você venceu.")
 else:
-    print("Infelizmente você perdeu.\nOnúmero escolhido pelo computador é: {}" .format(num_escolhido))
+    print("Infelizmente você perdeu.\nO número escolhido pelo computador é: {} e você escolheu {}" .format(num_escolhido, digite))
+print("\n")
 """
 
 
@@ -20,9 +29,10 @@ else:
 # Exercício 029
 veloc = float(input("\n\nInforme a velocidade do carro: "))
 if veloc > 80:
-    print("Você foi multado, pois o limite da velocidade estipulado é 80 km/h.")
+    print("Você foi multado, pois o limite da velocidade estipulado é 80km/h.")
     multa = (veloc - 80) * 7
     print("A sua multa é de {:.2f} reais.\n" .format(multa))
+print("Tenha um bom dia, dirija com segurança.")
 """
 
 
@@ -33,6 +43,7 @@ if (num % 2) == 0:
     print("O número {} é par!" .format(num))
 else:
     print("O número {} é ímpar!" .format(num))
+print("\n")
 """
 
 """
@@ -47,15 +58,78 @@ else:
 """
 
 
+
 """
 # Exercício 032
-ano = int(input("\nInformee um ano qualquer: "))
-if (ano % 4) == 0:
+from datetime import date
+
+ano = int(input("\nQue anor quer analisar? Ci=oloque 0 para analisar o ano atula: "))
+
+if ano == 0:
+    ano = date.today().year
+    
+if (ano % 4) == 0 and (ano % 100) != 0 or (ano % 400) == 0:
     print("O ano {} é bissexto, pois é divisível por quatro.\n" .format(ano))
+else:
+    print("O ano {} NÂO é bissexto.\n" .format(ano))
 """
     
+
+
 # Exercício 033
+print("\n\n")
 n1 = int(input("Digite o 1º valor: "))
 n2 = int(input("Digite o 2º valor: "))
 n3 = int(input("Digite o 3º valor: "))
-print("Verificando qual é o maior.")
+
+print("\n\nVerificando qual é o maior entre {}, {} e {}." .format(n1, n2, n3))
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+elif n3 > n1 and n3 > n2:
+    maior = n3
+print("O maior número é {}" .format(maior))
+
+print("\nVerificando qual é o menor entre {}, {} e {}." .format(n1, n2, n3))
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+elif n3 < n1 and n3 < n2:
+    menor = n3
+print("O menor número é {}" .format(menor))
+print("\n\n")
+
+
+
+"""
+# Exercício 034
+salario = float(input("\nQual é o seu salário? "))
+print("Calculando o seu aumento:")
+
+if salario > 1250:
+    salario_aumentado = salario + ((salario * 10)/100)
+    print("O seu salário era {:.2f} reais, você recebeu aumento de 10%, logo o seu novo salário é {:.2f} reais.\n" .format(salario, salario_aumentado))
+else:
+    salario_aumentado = salario + ((salario * 15)/100)
+    print("O seu salário era {:.2f} reais, você recebeu aumento de 15%, logo o seu novo salário é {:.2f} reais.\n" .format(salario, salario_aumentado))
+"""
+
+"""
+# Exercício 035
+r1 = float(input("Infrome a mediada da reta 1: "))
+r2 = float(input("Infrome a mediada da reta 2: "))
+r3 = float(input("Infrome a mediada da reta 3: "))
+
+print("\nVerificando se as três reetas podem formar triângulo:")
+if r1 < r3 and r2 < r3 and (r1 + r2) > r3:
+        print("As três reetas podem formar triângulo.")
+
+elif r1 < r2 and r3 < r2 and (r1 + r3) > r2:
+        print("As três retas podem formar triângulo.")
+
+elif r2 < r1 and r3 < r1 and (r2 + r3) > r1:
+        print("As três retas podem formar triângulo.")
+else:
+    print("As três retas NÂO podem formar triângulo.") 
+print("\n\n")
+"""
